2872766b0230fff319abc41e25b2997b484e7098
Matías Wasiak <matiaswasiak@gmail.com>
2020-11-12 23:20:20 -0300

    7. How to build a Headless CMS with TypeScript, React, GraphQL and Node: Sass Implementation

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,7 @@
 // Dependencies
 import React from 'react'
+import styles from './index.scss'
 
-export default (): React.ReactNode => <h1>Index Page</h1>
+export default (): React.ReactNode => (
+  <h1 className={styles.home}>Index Page</h1>
+)
